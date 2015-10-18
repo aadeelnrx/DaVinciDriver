b3485c65cbfcc2fd5fc5f0210a5a7d376e8fcaee
eedala <github@kuxi.de>
2015-10-18 18:41:46 +0200

    Boot time reduced, improved PID loop, radio link
    Booting time is reduced/improved by re-arranging
    the code and removing unnecessary delay() functions.
    
    The PID loop now updates with 100 Hz (was 10 Hz),
    while the measurement/logging loop is still at 10 Hz.
    This results in a much smoother drive, no pumping.
    PID parameters also tweaked a little.
    
    The radio link now uses RadioHead's library which
    comes stock with Teensyduino.
    A more flexible message structure is used now.

--- DaVinciDriver/DaVinciDriver.ino
+++ DaVinciDriver/DaVinciDriver.ino
@@ -14,22 +14,25 @@
 
 #include <SD.h>
 #include <IRremote.h>
+#include <Metro.h>
 #include <PID_v1.h>
-#include <RF24.h>
 #include <SPI.h>
+#include <RH_NRF24.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 
 /*-----( Declare Constants and Pin Numbers )-----*/
 
-// A simple data logger for the Arduino analog pins
-#define LOG_INTERVAL    100 //75 // mills between entries
-#define ECHO_TO_SERIAL   1 // echo data to serial port
-#define WAIT_TO_START    0 // Wait for serial input in setup()
-#define RADIO_ON         0 // Radio transmission
-#define IR_ON            0 // IR transmission
-#define SD_CARD_ON       0 // Logging to SD card
+#define LOG_INTERVAL    100 // mills between logging
+#define PID_INTERVAL     10 // mills between PID updates
+#define ECHO_TO_SERIAL   1  // echo data to serial port
+#define WAIT_TO_START    0  // Wait for serial input in setup()
+#define RADIO_ON         1  // Radio transmission
+#define IR_ON            0  // IR transmission
+#define SD_CARD_ON       0  // Logging to SD card
+
+#define STOP_AFTER_SECONDS 120
 
 // the digital pins that connect to the LEDs
 #define red_LED_PIN 13
@@ -53,7 +56,7 @@
 #define motor_In1_PIN 23
 #define motor_In2_PIN 22
 #define motor_PWM_PIN 21
-#define motor_stby_PIN 20
+#define motor_STBY_PIN 20
 
 // Motor PWM Frequency
 // To calculate you need R and L of the motor.
@@ -65,20 +68,29 @@
 // PWM_FREQUENCY = 1/Tmin
 #define PWM_FREQUENCY  400 // in Hz (guess, I haven't measured R and L)
 
+#define MOTOR_TARGET_SPEED 28
+#define MOTOR_P 1.5
+#define MOTOR_I 1.0
+#define MOTOR_D 0.1
+
+
 /*-------------------( Global Variables )-----------------------------------*/
-uint32_t starttime;
+// When to stop the car so that it doesn't drive endlessly during test:
+uint32_t stoptime;
+
+// Timers that control the update rates of various things in the main loop:
+// Units are milliseconds.
+Metro pid_timer = Metro(PID_INTERVAL);
+Metro measurement_timer = Metro(LOG_INTERVAL);
 
 // PID Controller
 double PID_setpoint, PID_input, PID_output;
-PID PIDcontroller(&PID_input, &PID_output, &PID_setpoint, 1.0, 10, 0, DIRECT);
+PID PIDcontroller(&PID_input, &PID_output, &PID_setpoint, MOTOR_P, MOTOR_I, MOTOR_D, DIRECT);
 
 //RTC_DS1307 RTC; // define the Real Time Clock object
 
-// Radio pipe addresses for the 2 nodes to communicate.
-const uint64_t pipes[2] = {0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL};
-
 // Set up nRF24L01 radio on SPI bus
-RF24 radio(CE_Radio_PIN, CSN_Radio_PIN);
+RH_NRF24 radio(CE_Radio_PIN, CSN_Radio_PIN);
 
 // Acceleration and orientation
 sensors_event_t accel_orient;
@@ -104,17 +116,42 @@ imu::Quaternion quat;
 File logfile;
 #endif
 
-// Structure of our telemetry message
-struct message_t {
+//----------------------------------------------
+// Structure of our telemetry messages
+// !!! Max. length is 28 bytes !!!
+// sizeof(uint32_t) = 4 bytes
+enum MSG_TYPES {
+  MSG_MEASUREMENT = 0,
+  MSG_TEXT = 1,
+};
+
+struct measurement_t{
+  uint32_t seq_no;
   uint32_t millis;
-  int speed;
-  int direction;
-  int voltage;
+  uint32_t speed;
+  uint32_t direction;
+  uint32_t voltage;
+};
+
+struct message_t {
+  MSG_TYPES msg_type;  // 0 = measurement, 1 = text
+  union {
+    measurement_t measurement;
+    char text[20];
+  } msg;
 };
 message_t message;
+measurement_t measurement;
+char text[26];
+//-----------------------------------------------
+
+// Sequence number for radio messages to detect message loss
+uint32_t seq_no;
 
 // Wheel encoder, updated by an interrupt routine
-volatile unsigned int speedCounter = 0;
+volatile uint32_t speedCounter = 0;
+uint32_t speedReading;
+uint32_t sp, sp1, sp2, sp3, sp4;  // last 4 values of speedReading
 
 //---------------------------------------------------------------
 // Macros to log data to either serial and/or SD-card.
@@ -126,9 +163,11 @@ volatile unsigned int speedCounter = 0;
 //    LOG_LN(x)  to both
 #if ECHO_TO_SERIAL 
 #define LOG_SERIAL_LN(x) Serial.println(x);
+#define LOG_SERIAL_LN2(x,y) Serial.println(x,y);
 #define LOG_SERIAL(x) Serial.print(x);
 #else
 #define LOG_SERIAL_LN(x)
+#define LOG_SERIAL_LN2(x,y)
 #define LOG_SERIAL(x)
 #endif
 
@@ -240,7 +279,7 @@ void translateIR() // takes action based on IR code received
 
   default: 
     LOG_SERIAL(" other button   :");
-    LOG_SERIAL_LN(results.value, HEX);
+    LOG_SERIAL_LN2(results.value, HEX);
   }// End Case
 } //END translateIR
 #endif
@@ -305,21 +344,65 @@ void setup(void)
   pinMode(motor_In1_PIN, OUTPUT);
   pinMode(motor_In2_PIN, OUTPUT);
   pinMode(motor_PWM_PIN, OUTPUT); 
-  pinMode(motor_stby_PIN, OUTPUT); 
+  pinMode(motor_STBY_PIN, OUTPUT); 
+  digitalWrite(motor_STBY_PIN, HIGH);
+  // Set PWM frequency.  The motor diver can handle 100 kHz max.
+  // Setting the frequency on one pin changes it for all other
+  // pins on this timer as well (5, 6, 9, 10, 20, 21, 22, 23)
+  analogWriteFrequency(motor_In1_PIN, PWM_FREQUENCY);
   
-  digitalWrite(motor_stby_PIN, HIGH);
+  // PID
+  PID_input = 0.0;
+  PID_setpoint = MOTOR_TARGET_SPEED;
+  PIDcontroller.SetMode(AUTOMATIC);
+  PIDcontroller.SetSampleTime(10);
+  sp1 = sp2 = sp3 = sp4 = 0;    
   
   // The default SCK pin is connected to the LED which we use for something else
   // Required for Radio/NRF24L01+ and SD-card
+  // !!! Must be before SPI.begin() if that is used !!!
   SPI.setSCK(SCK_PIN);
   
   digitalWrite(green_LED_PIN, HIGH);
 
   // initialize the serial communication:
   Serial.begin(115200);
-  delay(5000); // otherwise first lines may be missing
-  Serial.println("DaVinciDriver");
+
+  // Blink to indicate it's going to start:
+  for (int i=0; i < 4; i++)
+  {
+    digitalWrite(green_LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(green_LED_PIN, LOW);
+    delay(500);
+  }
+  LOG_SERIAL_LN("DaVinciDriver");
+
+  // BNO055 IMU initialisation
+  // Done here (4 seconds after serial initialisation) so that
+  // we can print to serial here.
+  LOG_SERIAL_LN("Before bno begin");
+  if (!bno.begin())
+  {
+    Serial.println("bno error");
+    //There was a problem detecting the BNO055 ... check your connections
+    error("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+  }
+  LOG_SERIAL_LN("After bno begin");
+
+  // last second: fast blinking
+  // The BNO055 needs one second to initialise anyway.
+  for (int i=0; i < 10; i++)
+  {
+    digitalWrite(green_LED_PIN, HIGH);
+    delay(50);
+    digitalWrite(green_LED_PIN, LOW);
+    delay(50);
+  }
   
+  // This has to be done at least one second after bno.begin():
+  bno.setExtCrystalUse(true);  
+
 
 #if WAIT_TO_START
   Serial.println("Type any character to start");
@@ -328,15 +411,15 @@ void setup(void)
 
 #if SD_CARD_ON
   // initialize the SD card
-  Serial.print("Initializing SD card...");
+  LOG_SERIAL("Initializing SD card...");
 
   // see if the card is present and can be initialized:
   if (!SD.begin(CSN_CF_PIN)) {
-    Serial.println("Card failed, or not present");
+    LOG_SERIAL_LN("Card failed, or not present");
     // don't do anything more:
     return;
   }
-  Serial.println("card initialized.");
+  LOG_SERIAL_LN("card initialized.");
   
   // create a new file
   char filename[] = "LOGGER00.CSV";
@@ -354,60 +437,31 @@ void setup(void)
     error("couldnt create file");
   }
 
-  Serial.print("Logging to: ");
-  Serial.println(filename);
+  LOG_SERIAL("Logging to: ");
+  LOG_SERIAL_LN(filename);
 #endif
 
 #if RADIO_ON
   // Initialize all radio related modules
-  Serial.println("Initializing radio...");
-  radio.begin();
+  LOG_SERIAL_LN("Initializing radio...");
+  seq_no = 0;
+  
+  //radio.begin();
 
-  // optionally, increase the delay between retries & # of retries
-  radio.setRetries(15,15);
-
-  Serial.println(F("Setting up channel"));
-  // optionally, reduce the payload size.  seems to
-  // improve reliability
-  radio.setPayloadSize(sizeof(message_t));
-  radio.setChannel(0x4c);
-  radio.setPALevel(RF24_PA_MAX);
-
-  // Open pipes to other node for communication
-  // Open 'our' pipe for writing
-  // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-  radio.openWritingPipe(pipes[0]);
-  radio.openReadingPipe(1,pipes[1]);
-
-  // Start listening
-  radio.startListening();
-
-  // Dump the configuration of the rf unit for debugging
-  //
-  //radio.printDetails();
-  Serial.println(F("Radio ready."));
+  if (! radio.init())
+    LOG_SERIAL_LN("init failed");
+  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (! radio.setChannel(1))
+    LOG_SERIAL_LN("setChannel failed");
+  if (! radio.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    LOG_SERIAL_LN("setRF failed");   
+   
+  LOG_SERIAL_LN(F("Radio ready."));
 #endif //RADIO_ON
 
 #if IR_ON  
   irrecv.enableIRIn(); // Start the IR receiver
 #endif
-
-  // Set PWM frequency.  The motor diver can handle 100 kHz max.
-  // Setting the frequency on one pin changes it for all other
-  // pins on this timer as well (5, 6, 9, 10, 20, 21, 22, 23)
-  analogWriteFrequency(motor_In1_PIN, PWM_FREQUENCY);
-Serial.println("Before bno begin");
-  if (!bno.begin())
-  {
-Serial.println("bno error");
-    //There was a problem detecting the BNO055 ... check your connections
-    LOG_SDCARD_LN("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!")
-    error("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-  }
-Serial.println("After bno begin");
-
-  delay(1000);
-  bno.setExtCrystalUse(true);  
 
 #if SD_CARD_ON
   logfile.println("Millis,Delay,Time,Ori.x,Ori.y,Ori.z,Acc.x,Acc.y,Acc.z,LAcc.x,LAcc.y,LAcc.z,Grav.x,Grav.y,Grav.z,Mag.x,Mag.y,Mag.z,Gyro.x,Gyro.y,Gyro.z,Euler.x,Euler.y,Euler.z,Quat.w,Quat.x,Quat.y,Quat.z,Speed,Direction,VoltageIn,VoltageEngine");    
@@ -431,28 +485,8 @@ Serial.println("After bno begin");
   //analogReference(EXTERNAL);
 #endif
 
-  // Blink to indicate it's going to start:
-  for (int i=0; i < 4; i++)
-  {
-    digitalWrite(green_LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(green_LED_PIN, LOW);
-    delay(500);
-  }
-  // last second: fast blinking
-  for (int i=0; i < 10; i++)
-  {
-    digitalWrite(green_LED_PIN, HIGH);
-    delay(50);
-    digitalWrite(green_LED_PIN, LOW);
-    delay(50);
-  }
-  
-  // PID
-  PID_input = 0.0;
-  PID_setpoint = 40;
-  PIDcontroller.SetMode(AUTOMATIC);
-  
+  // Stop the car after some time
+  stoptime = millis() + STOP_AFTER_SECONDS * 1000;
 }
 
 
@@ -461,130 +495,166 @@ Serial.println("After bno begin");
 void loop(void)
 {
   // Stop motor after some time
-  if ((millis() - starttime) > 600 * 1000)
+  if (millis() >= stoptime)
   {
     motor_off_brake();
     while(1);
   }
   
-  // Delay for the amount of time we want between readings
-  uint32_t delayTime = (LOG_INTERVAL -1) - (millis() % LOG_INTERVAL);
-  delay(delayTime);
+  // Higher frequency timer for the PID controller
+  if (pid_timer.check())
+  {
+    digitalWrite(green_LED_PIN, HIGH);
+    
+    // Take all measurement values right at the beginning of the
+    // main loop to ensure taking them after the same interval
+    // all the time
+    noInterrupts();
+    speedReading = speedCounter;  //calculate speed based on speedCounter
+    speedCounter = 0;                 //reset speedCounter 
+    interrupts();
 
-  // Log milliseconds since starting
-  uint32_t msec_since_start = millis();
+    sp = speedReading + sp1 + sp2 + sp3 + sp4;
+
+    // Motor also as early as possible to ensure constant update intervals
+    PID_input = (float)sp;
+    PIDcontroller.Compute();
+    motor_on_pwm(PID_output);
+
+    // update history:
+    sp1 = sp2;
+    sp2 = sp3;
+    sp3 = sp4;
+    sp4 = speedReading;
+
+    digitalWrite(green_LED_PIN, LOW);  
+  }
   
-  digitalWrite(green_LED_PIN, HIGH);
+  if (measurement_timer.check())
+  {
+    digitalWrite(green_LED_PIN, HIGH);
 
-  // Take all measurement values right at the beginning of the
-  // main loop to ensure taking them after the same interval
-  // all the time
-  noInterrupts();
-  int speedReading = speedCounter;  //calculate speed based on speedCounter
-  speedCounter = 0;                 //reset speedCounter 
-  interrupts();
+    // Log milliseconds since starting
+    uint32_t msec_since_start = millis();
   
-  int trackVoltReading = analogRead(track_volt_PIN);    
-  int engineVoltReading = analogRead(engine_volt_PIN);    
-
-  // Read the acceleration and orientation:
-  bno.getEvent(&accel_orient);
-
-  int dirReading = int(accel_orient.orientation.x);
-
-  // Possible vector values can be:
-  // - VECTOR_ACCELEROMETER - m/s^2
-  // - VECTOR_MAGNETOMETER  - uT
-  // - VECTOR_GYROSCOPE     - rad/s
-  // - VECTOR_EULER         - degrees
-  // - VECTOR_LINEARACCEL   - m/s^2
-  // - VECTOR_GRAVITY       - m/s^2
-  acceleration  = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  lacceleration = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  gravity       = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
-  magnometer    = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  gyroscope     = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  euler         = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  quat          = bno.getQuat();
-
-  // Motor also as early as possible to ensure constant update intervals
-  PID_input = (float)speedReading;
-  PIDcontroller.Compute();
-  motor_on_pwm(PID_output);
+    // Take all measurement values right at the beginning of the
+    // main loop to ensure taking them after the same interval
+    // all the time
+    int trackVoltReading = analogRead(track_volt_PIN);    
+    int engineVoltReading = analogRead(engine_volt_PIN);    
+  
+    // Read the acceleration and orientation:
+    bno.getEvent(&accel_orient);
+  
+    uint32_t dirReading = uint32_t(accel_orient.orientation.x);
+  
+    // Possible vector values can be:
+    // - VECTOR_ACCELEROMETER - m/s^2
+    // - VECTOR_MAGNETOMETER  - uT
+    // - VECTOR_GYROSCOPE     - rad/s
+    // - VECTOR_EULER         - degrees
+    // - VECTOR_LINEARACCEL   - m/s^2
+    // - VECTOR_GRAVITY       - m/s^2
+    acceleration  = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    lacceleration = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+    gravity       = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+    magnometer    = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+    gyroscope     = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+    euler         = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    quat          = bno.getQuat();
   
   
 #if IR_ON
-  if (irrecv.decode(&results)) // have we received an IR signal?
-  {
+    if (irrecv.decode(&results)) // have we received an IR signal?
+    {
 //    LOG_SERIAL_LN(results.value, HEX);  UN Comment to see raw values
-    translateIR(); 
-    irrecv.resume(); // receive the next value
-  }  
+      translateIR(); 
+      irrecv.resume(); // receive the next value
+    }  
 #endif
 
-
-  // Log measurements to serial and/or flash:
-  LOG(msec_since_start)
-  LOG(delayTime)
   
-  LOG(accel_orient.orientation.x)
-  LOG(accel_orient.orientation.y)
-  LOG(accel_orient.orientation.z)
-  
-  LOG(acceleration.x())
-  LOG(acceleration.y())
-  LOG(acceleration.z())
-  
-  LOG(lacceleration.x())
-  LOG(lacceleration.y())
-  LOG(lacceleration.z())
-  
-  LOG(gravity.x())
-  LOG(gravity.y())
-  LOG(gravity.z())
-  
-  LOG(magnometer.x())
-  LOG(magnometer.y())
-  LOG(magnometer.z())
-  
-  LOG(gyroscope.x())
-  LOG(gyroscope.y())
-  LOG(gyroscope.z())
-  
-  LOG(euler.x())
-  LOG(euler.y())
-  LOG(euler.z())
-  
-  LOG(quat.w())
-  LOG(quat.x())
-  LOG(quat.y())
-  LOG(quat.z())
-  
-  LOG(speedReading)
-  LOG(dirReading)
-  LOG(trackVoltReading)    
-  LOG(engineVoltReading)
-  LOG_LN()
-  
+    // Log measurements to serial and/or flash:
+    LOG(msec_since_start)
+    
+    LOG(accel_orient.orientation.x)
+    LOG(accel_orient.orientation.y)
+    LOG(accel_orient.orientation.z)
+    
+    LOG(acceleration.x())
+    LOG(acceleration.y())
+    LOG(acceleration.z())
+    
+    LOG(lacceleration.x())
+    LOG(lacceleration.y())
+    LOG(lacceleration.z())
+    
+    LOG(gravity.x())
+    LOG(gravity.y())
+    LOG(gravity.z())
+    
+    LOG(magnometer.x())
+    LOG(magnometer.y())
+    LOG(magnometer.z())
+    
+    LOG(gyroscope.x())
+    LOG(gyroscope.y())
+    LOG(gyroscope.z())
+    
+    LOG(euler.x())
+    LOG(euler.y())
+    LOG(euler.z())
+    
+    LOG(quat.w())
+    LOG(quat.x())
+    LOG(quat.y())
+    LOG(quat.z())
+    
+    LOG(speedReading)
+    LOG(dirReading)
+    LOG(trackVoltReading)    
+    LOG(engineVoltReading)
+    LOG_LN()
+    
 #if SD_CARD_ON
-  logfile.flush();
+    logfile.flush();
 #endif
 
 #if RADIO_ON
-  // Construct the message we'll send
-  message = (message_t){msec_since_start, speedReading, dirReading, trackVoltReading};
+    seq_no++;
+    // If we check here that the packet was sent from the previous loop,
+    // we save time because we could already do lots of other things in the
+    // meantime
+  //  if (seq_no > 1) radio.waitPacketSent();
+ 
+    // Construct the message we'll send
+    message.msg_type = MSG_MEASUREMENT;
+    message.msg.measurement = (measurement_t){seq_no, msec_since_start, speedReading, dirReading, sp /*trackVoltReading*/};
   
-  // We have to stop/start listening in order to receive ACK packets
-  radio.stopListening();
-
-  //LOG_SERIAL_LN(F("Transmitting on radio"));
-  radio.write(&message, sizeof(message_t) );
-  //bool ok = radio.write(&message, sizeof(message_t) );
-  //if (ok)  LOG_SERIAL_LN(F("ok..."));
-  //else     LOG_SERIAL_LN(F("failed.\n"));
-  radio.startListening();
+    LOG_SERIAL_LN(F("Transmitting on radio"));
+    radio.send((uint8_t *)&message, sizeof(message_t));
+    radio.waitPacketSent();
+ 
+    // Did we receive something from the PC? 
+    uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    while (radio.waitAvailableTimeout(0))
+    { 
+      // Should be a reply message for us now   
+      if (radio.recv(buf, &len))
+      {
+  //      Serial.print("got reply: ");
+  //      Serial.println((char*)buf);
+      }
+      else
+      {
+  //      Serial.println("recv failed");
+      }
+   
+   }
 #endif //RADIO_ON
 
-  digitalWrite(green_LED_PIN, LOW);
+    digitalWrite(green_LED_PIN, LOW);
+  } // measurement updates
 } 
 
